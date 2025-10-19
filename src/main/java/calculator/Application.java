c16297b2fe952e48dc69ad8917cb50f5837ca198
chaeros <chpark6851@koreatech.ac.kr>
2025-10-19 23:41:53 +0900

    refactor: Scanner 대신 MissionUtils Console API로 입력 방식 변경
    - 기존 Scanner 입력 방식을 camp.nextstep.edu.missionutils.Console.readLine()으로 교체

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,12 +1,11 @@
 package calculator;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
+        String input = Console.readLine();
         int result = StringSplitAndAddCalculator.add(input);
         System.out.println("결과 : " + result);
     }
