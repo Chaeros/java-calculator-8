6f860a00c7328cae2412db6f84ec5e9481374ab4
chaeros <chpark6851@koreatech.ac.kr>
2025-10-19 16:56:18 +0900

    feat : 문자열 입력단 추가 및 문자열 처리 Calculator 클래스 생성
    - 입력한 문자열을 처리 후, 결과를 숫자로 반환하는 add 메서드 추가
    - 빈 문자열 입력 시 0 반환
    - 예외 발생 시, 예외 내용 Console 출력

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        try (Scanner sc = new Scanner(System.in)) {
+            String input = sc.nextLine();
+            int result = StringSplitAndAddCalculator.add(input);
+            System.out.println("결과 : " + result);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/calculator/StringSplitAndAddCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringSplitAndAddCalculator {
+    public static int add(String input) {
+        if (input == null || input.isBlank()) {
+            return 0;
+        }
+        return 0;
+    }
+}
