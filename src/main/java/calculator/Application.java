dfad9a7be40530a52edcf737a9b88a72b17cc986
chaeros <chpark6851@koreatech.ac.kr>
2025-10-19 17:31:35 +0900

    refactor: 예외 처리를 main에서 제거하여 테스트 검증 가능하도록 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        try (Scanner sc = new Scanner(System.in)) {
-            String input = sc.nextLine();
-            int result = StringSplitAndAddCalculator.add(input);
-            System.out.println("결과 : " + result);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        int result = StringSplitAndAddCalculator.add(input);
+        System.out.println("결과 : " + result);
     }
 }
